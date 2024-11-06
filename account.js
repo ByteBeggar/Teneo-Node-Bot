5a36f0591b3a9b6d3f735e7d9d82cceb34627403
Byte Beggar <rahmacutee07@gmail.com>
2024-11-06 18:52:01 +0800

    Update account.js

--- account.js
+++ account.js
@@ -1,6 +1,8 @@
 module.exports = [
   
-"giyourdiewantluxih@gmail.com,gvmkADbGdikEH6",
+"name1@gmail.com,123456",
+"name2@gmail.com,123456",
+"name3@gmail.com,123456"
       
     // Add more accounts
 ];
